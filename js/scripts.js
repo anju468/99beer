7a1112ca2397c5ceea6309a02a6d0f823f19163d
Anju <Anju@Anjus-MacBook-Air.local>
2015-10-25 15:04:26 -0700

    add second successful spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 
 var beer = function(userInput) {
+  if (userInput === 1) {
+    return "1 bottle of beer on the wall, 1 bottle of beer. Take it down and pass it around, no more bottles of beer on the wall.";
+  }
+
   if (userInput === 0) {
     return "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.";
   }
--- specs/specs.js
+++ specs/specs.js
@@ -2,4 +2,9 @@ describe('beer', function() {
   it("will return false if input is 0", function() {
     expect(beer(0)).to.equal("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
   });
+
+  it("will return second last line of 99 beer when 1 is input", function() {
+    expect(beer(1)).to.equal("1 bottle of beer on the wall, 1 bottle of beer. Take it down and pass it around, no more bottles of beer on the wall.");
+  });
+
 });
